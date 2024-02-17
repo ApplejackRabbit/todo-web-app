8008260fcb4abc6f55f0416b2bb340fd77ddd67e
ApplejackRabbit <140619457+ApplejackRabbit@users.noreply.github.com>
2024-02-18 01:27:45 +0800

    feat: add default error handlers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import type { ErrorRequestHandler } from "express";
 import todoRouter from "./routes/todoRoute";
+
+const jsonErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+};
 
 const app = express();
 
@@ -10,5 +15,11 @@ app.get("/", (req, res) => {
 });
 
 app.use("/todo", todoRouter);
+
+app.get("*", (req, res) => {
+  res.status(404).json({});
+});
+
+app.use(jsonErrorHandler);
 
 export default app;
